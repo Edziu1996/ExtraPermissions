b25c02735b81fb3d92b2a3a271f11a839d2033ab
Edziu1996 <edziu1996@gmail.com>
2016-01-27 22:27:43 +0100

    Version

--- src/main/java/com/gmail/edziu1996/extrapermissions/ExtraPermissions.java
+++ src/main/java/com/gmail/edziu1996/extrapermissions/ExtraPermissions.java
@@ -105,7 +105,7 @@ public class ExtraPermissions
 	{
 		public static final String ID = "ExtraPerm";
 		public static final String NAME = "ExtraPermissions";
-		public static final String VERSION = "0.2.4";
+		public static final String VERSION = "0.2.8";
 		public static final String DEPENDENCIES = "required-after:NameAPI@[0.2,)";
 	}
 }
